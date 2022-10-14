a696dceb1a370e2adc660465596ce30c27568ba0
twozeronine <amdusias209@gmail.com>
2022-10-14 23:29:35 +0900

    fix : 오탈자 때문에 생긴 버그 수정

--- config/test.exs
+++ config/test.exs
@@ -30,4 +30,4 @@ config :rumbl, Rumbl.Mailer, adapter: Swoosh.Adapters.Test
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
 
-config :pdkdf2_elixir, :rounds, 1
+config :pbkdf2_elixir, :rounds, 1
