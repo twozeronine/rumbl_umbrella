18e838796f613ffffaca048c4277c2932fb5faa2
twozeronine <amdusias209@gmail.com>
2022-10-15 14:05:09 +0900

    feat : supervision 동작 확인을 위해 tick 구현

--- apps/info_sys/lib/info_sys/counter.ex
+++ apps/info_sys/lib/info_sys/counter.ex
@@ -13,6 +13,7 @@ defmodule InfoSys.Counter do
   end
 
   def init(initial_val) do
+    Process.send_after(self(), :tick, 1_000)
     {:ok, initial_val}
   end
 
@@ -26,5 +27,11 @@ defmodule InfoSys.Counter do
 
   def handle_call(:val, _from, val) do
     {:reply, val, val}
+  end
+
+  def handle_info(:tick, val) do
+    IO.puts("tick #{val}")
+    Process.send_after(self(), :tick, 1_000)
+    {:noreply, val - 1}
   end
 end
