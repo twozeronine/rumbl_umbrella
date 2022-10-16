ec988fde45eecf048c4f2894c78a1000d99bc2c8
twozeronine <amdusias209@gmail.com>
2022-10-16 13:31:57 +0900

    feat : add cache schedule

--- apps/info_sys/lib/info_sys/cache.ex
+++ apps/info_sys/lib/info_sys/cache.ex
@@ -1,6 +1,8 @@
 defmodule InfoSys.Cache do
   use GenServer
 
+  @clear_interval :timer.seconds(60)
+
   def put(name \\ __MODULE__, key, value) do
     true = :ets.insert(tab_name(name), {key, value})
     :ok
@@ -19,8 +21,23 @@ defmodule InfoSys.Cache do
 
   @impl true
   def init(opts) do
-    new_table(opts[:name])
-    {:ok, %{}}
+    state = %{
+      interval: opts[:clear_interval] || @clear_interval,
+      timer: nil,
+      table: new_table(opts[:name])
+    }
+
+    {:ok, schedule_clear(state)}
+  end
+
+  @impl true
+  def handle_info(:clear, state) do
+    :ets.delete_all_objects(state.table)
+    {:noreply, schedule_clear(state)}
+  end
+
+  defp schedule_clear(state) do
+    %{state | timer: Process.send_after(self(), :clear, state.interval)}
   end
 
   defp new_table(name) do
